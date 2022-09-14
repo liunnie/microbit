06651017dfe176220c7e701166acbec9e8e3d6dd
liunnie <113489302+liunnie@users.noreply.github.com>
2022-09-14 11:43:19 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,64 @@
+input.onButtonPressed(Button.A, function () {
+    if (1000 >= steps && 5000 < steps) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . # # . .
+            . # # . .
+            . . . . .
+            `)
+        basic.showString("GOOD")
+    } else if (5000 >= steps && 10000 < steps) {
+        basic.showLeds(`
+            . . . . .
+            . # . . .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        basic.showString("GREAT")
+    } else if (10000 <= steps) {
+        basic.showLeds(`
+            . # . . .
+            . # . . .
+            . # # # .
+            . # # # .
+            . # # # .
+            `)
+        basic.showString("HURRY UP")
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . # # # .
+            . # # # .
+            . . . # .
+            . . . . .
+            `)
+        basic.showString("HURRY UP")
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    steps += 1
+    basic.showNumber(steps)
+})
+input.onButtonPressed(Button.AB, function () {
+    steps = 0
+    basic.showString("RESET")
+    music.playTone(523, music.beat(BeatFraction.Quarter))
+    music.playTone(392, music.beat(BeatFraction.Quarter))
+    music.playTone(330, music.beat(BeatFraction.Quarter))
+    music.playTone(262, music.beat(BeatFraction.Half))
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(0.23 * steps)
+    basic.showString("kcal")
+    basic.showLeds(`
+        . . # . .
+        # . # . #
+        # # . # #
+        # . . . #
+        . # # # .
+        `)
+})
+let steps = 0
+steps = 0
